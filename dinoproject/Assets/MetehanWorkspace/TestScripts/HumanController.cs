afde6d342effb60ed29df15fc3224a7230af1e83
21070006210 <65510727+metehanugus@users.noreply.github.com>
2024-03-13 18:56:25 +0300

    NavMesh + Dinozor, Human NPC
    Navmesh eklendi
    Dinozor statları eklendi
    Dinozor playere gidiyor yol üstünde insanları kovalıyor.

---
+++ dinoproject/Assets/MetehanWorkspace/TestScripts/HumanController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class HumanController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public float panicDistance = 1f; // Dinozorları algılama mesafesi
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        GameObject[] dinosaurs = GameObject.FindGameObjectsWithTag("Dinosaur");
+        foreach (var dino in dinosaurs)
+        {
+            if (Vector3.Distance(transform.position, dino.transform.position) <= panicDistance)
+            {
+                Debug.Log("Dinozordan kaç dinodan kaç!");
+                Vector3 fleeDirection = transform.position - dino.transform.position;
+                Vector3 newGoal = transform.position + fleeDirection.normalized * panicDistance;
+                agent.SetDestination(newGoal);
+                break; // En yakın dinozordan kaç
+            }
+        }
+    }
+}
